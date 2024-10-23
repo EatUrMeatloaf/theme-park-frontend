47f4fc2ad71532e9898ad5d3355543158bc5d05b
EC2 Default User <ec2-user@ip-10-0-5-62.ec2.internal>
2024-10-23 22:57:07 +0000

    Module 2

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@ export const appConfig = {
   initStateAPI: 'https://avopsl6qz4.execute-api.us-east-1.amazonaws.com/Prod/InitState/', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
   // MODULE 2 - REALTIME
   iot: {
-    poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
-    host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: '' // e.g. 'us-west-1'
+    poolId: 'us-east-1:85c2e37f-b72d-4791-b0ad-e1a334a164f2', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
+    host: 'aqs5isnspcj3x-ats.iot.us-east-1.amazonaws.com', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
+    region: 'us-east-1' // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
